aaa43febba41f6d155cf3c1af64c3317df0c6ace
_ <53088156+636f7374@users.noreply.github.com>
2021-05-21 04:29:02 +0800

    Improve maximumNumberOfRetriesForPerIpAddress.

--- src/dns/extra/socket/tcp_socket.cr
+++ src/dns/extra/socket/tcp_socket.cr
@@ -1,7 +1,8 @@
 class TCPSocket < IPSocket
   def initialize(ip_address : IPAddress, dns_timeout = nil, connect_timeout = nil)
-    Addrinfo.build_tcp ip_address do |addrinfo|
+    Addrinfo.build_tcp ip_address: ip_address do |addrinfo|
       super addrinfo.family, addrinfo.type, addrinfo.protocol
+
       connect(addrinfo, timeout: connect_timeout) do |error|
         close
         error
@@ -12,9 +13,6 @@ class TCPSocket < IPSocket
   def self.new(host : String, port : Int32, dns_resolver : DNS::Resolver, connect_timeout : Int | Time::Span | Nil = nil) : TCPSocket
     fetch_type, ip_addresses = dns_resolver.getaddrinfo host: host, port: port
     raise Exception.new String.build { |io| io << "TCPSocket.new: Unfortunately, DNS::Resolver.getaddrinfo! The host: (" << host << ") & fetchType: (" << fetch_type << ")" << " IPAddress result is empty!" } if ip_addresses.empty?
-
-    maximum_number_of_retries_for_per_ip_address = dns_resolver.options.socket.maximumNumberOfRetriesForPerIpAddress
-    maximum_number_of_retries_for_per_ip_address = 1_u8 if maximum_number_of_retries_for_per_ip_address <= 0_u8
 
     connect_timeout_time_span = 10_i32.seconds
     connect_timeout_time_span = connect_timeout if connect_timeout.is_a? Time::Span
@@ -41,24 +39,7 @@ class TCPSocket < IPSocket
       end
 
       begin
-        socket = maximum_number_of_retries_for_per_ip_address.times do |time|
-          _starting_time = Time.local
-
-          begin
-            _socket = new ip_address: ip_address, connect_timeout: attempt_connect_timeout_span
-          rescue ex
-            unless (time + 1_i32) == maximum_number_of_retries_for_per_ip_address
-              next if ex.is_a? IO::Error && "Error reading socket: Connection reset by peer" == ex.message && ((Time.local - _starting_time) < 1_i32.seconds)
-            end
-
-            raise ex
-          end
-
-          next if _socket.closed? unless (time + 1_i32) == maximum_number_of_retries_for_per_ip_address
-          break _socket
-        end
-
-        raise Exception.new String.build { |io| io << "TCPSocket.new: IPAddress: (" << ip_address << ") connection failed (Nil)!" } unless socket
+        socket = attempt_create_socket! dns_resolver: dns_resolver, fetch_type: fetch_type, ip_address: ip_address, connect_timeout: attempt_connect_timeout_span
       rescue ex
         dns_resolver.__create_socket_exception_call ip_address: ip_address, exception: ex
 
@@ -90,5 +71,26 @@ class TCPSocket < IPSocket
     end
 
     raise Exception.new String.build { |io| io << "TCPSocket.new: Tries to connect the DNS::Resolver.getaddrinfo! address: (" << host << ':' << port << ") & fetchType: (" << fetch_type << ") & count: (" << ip_addresses.size << ") IP addresses, But still failed to connect!" }
+  end
+
+  private def self.attempt_create_socket!(dns_resolver : DNS::Resolver, fetch_type : DNS::FetchType, ip_address : Socket::IPAddress, connect_timeout : Time::Span) : TCPSocket
+    maximum_number_of_retries_for_per_ip_address = dns_resolver.options.socket.maximumNumberOfRetriesForPerIpAddress
+    maximum_number_of_retries_for_per_ip_address = 1_u8 if maximum_number_of_retries_for_per_ip_address <= 0_u8
+
+    maximum_number_of_retries_for_per_ip_address.times do |time|
+      _starting_time = Time.local
+      step = time + 1_i32
+
+      begin
+        _socket = new ip_address: ip_address, connect_timeout: connect_timeout
+
+        next if _socket.closed? && step != maximum_number_of_retries_for_per_ip_address
+        return _socket unless _socket.closed?
+      rescue ex
+        next
+      end
+    end
+
+    raise Exception.new String.build { |io| io << "TCPSocket.attempt_create_socket!: IPAddress: (" << ip_address << ") connection failed!" }
   end
 end
